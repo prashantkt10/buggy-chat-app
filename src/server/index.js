cf6082956d6690bc362349acfb2d697ac52b67a8
Prashant Kumar Tripathi <hello@prashant.live>
2020-05-29 15:17:25 +0530

    make it better

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,7 @@
 const http = require('http'), ws = require('ws'), fs = require('fs');
 
 const httpServer = http.createServer(function (req, res) {
-    fs.readFile('../static/index.html', function (err, data) {
+    fs.readFile('index.html', function (err, data) {
         if (err) res.end('Server error.');
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
